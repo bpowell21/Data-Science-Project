4815e863338a0c5ddf5e5925859d7bd260472eab
6119greend <35507334+6119greend@users.noreply.github.com>
2018-04-30 14:04:45 -0500

    Add files via upload

--- VOTES vs Education.R
+++ VOTES vs Education.R
@@ -1,3 +1,4 @@
+#Load library
 library(mice)
 library(VIM)
 library(ggplot2)
@@ -7,85 +8,64 @@ library(sm)
 library(tidyverse)
 library(dplyr)
 library(reshape2)
+install.packages("xlsx")
+library("xlsx")
 
 
-
+# load data set
 rata <- read.csv("county_facts.csv")
-
 
 rat <-read.csv("Votes per state.csv")
 
+#Get data set to where the states are only in set
+rata1 <- rata[rata$state_abbreviation == "",]
+rat1 <- rat[rat$Electoral..method =="WTA",]
 
+#Make a new data set about education by state
+Highschool <-c(rata1$EDU635213)
+Bachelord <-c(rata1$EDU685213)
+Highschoold <- Highschool-Bachelord
+State1<-rata1$area_name
+education<- data.frame(State1, Highschoold, Bachelord)
+education1<-education[c(3:52),]
+education1$ID <- 1:nrow(education1)
 
-highschool <-c(rata$EDU635213)
-bachelord <-c(rata$EDU685213)
-highschoold <- highschool-bachelord
-state1<-rata$area_name
-# state1[c("Alabama", 
-#          Alaska, 
-#          Arizona ,
-#          Arkansas ,
-#          California ,
-#          Colorado ,
-#          Connecticut, 
-#          Delaware, 
-#          Florida, 
-#          Georgia ,
-#          Hawaii ,
-#          Idaho ,
-#          Illinois, Indiana ,
-#          Iowa ,
-#          Kansas 
-#          Kentucky 
-#          Louisiana 
-#          Maine 
-#          Maryland 
-#          Massachusetts 
-#          Michigan 
-#          Minnesota 
-#          Mississippi 
-#          Missouri 
-#          Montana Nebraska 
-#          Nevada 
-#          New Hampshire 
-#          New Jersey 
-#          New Mexico 
-#          New York 
-#          North Carolina 
-#          North Dakota 
-#          Ohio 
-#          Oklahoma 
-#          Oregon 
-#          Pennsylvania 
-#          Rhode Island 
-#          South Carolina 
-#          South Dakota 
-#          Tennessee 
-#          Texas 
-#          Utah 
-#          Vermont 
-#          Virginia 
-#          Washington 
-#          West Virginia 
-#          Wisconsin 
-#          Wyoming
-# )]
-education<- data.frame(state1, highschoold, bachelord)
+#Make a new data set about vote by state
+state4 <- rat1$ï..State.or..district
+Hillary <- rat1$Hillary.Clinton..Democratic..
+Donald<- rat1$Donald.Trump..Republican..
+votes <- data.frame(state4, Hillary, Donald)
+votes$ID <- 1:nrow(votes)
 
-state <- rat$ï..State.or..district
-hillary <- rat$Hillary.Clinton..Democratic..
-Do<- rat$Donald.Trump..Republican..
-votes <- data.frame(state, hillary, Do)
-vote2 <-votes[c(1:19,22:29,33:55),]
-
-bar <- ggplot(data=dat, aes(x=state,y=bachelord))
+#Bar graph
+Viewbar <- ggplot(data=total, aes(x=State1,y=Bachelord))
 bar + geom_bar(stat="identity") + geom_text(aes(label=ba), vjust=1.6, color="Blue", size=3.5)+
   xlab("States") +  ylab("Percentage of individual with bachelor's degree or higher") + ggtitle("Bachelor's Degree Education of people who votes by States") + coord_flip()
 
-bar <- ggplot(data=dat, aes(x=state,y=highschoold))
+bar <- ggplot(data=total, aes(x=State1,y=Highschoold))
 bar + geom_bar(stat="identity") + geom_text(aes(label=ba), vjust=1.6, color="Blue", size=3.5)+
   xlab("States") +  ylab("Percentage of individual with bachelor's degree or higher") + ggtitle("Bachelor's Degree Education of people who votes by States") + coord_flip()
 
-
-total<-merge (education, vote2)
+# Merge data
+total<-merge (education1, votes, b.x = "ID")
 View(total)
+total$state4 <- NULL
+total$ID <- NULL
+View(total)
+
+#Bar graph
+bar1 <- ggplot(data=total, aes(x=State1,y=Bachelord))
+bar + geom_bar(stat="identity") + geom_text(aes(label=Bachelord), vjust=1.6, color="Blue", size=3.5)+
+  xlab("States") +  ylab("Percentage of individual with bachelor's degree or higher") + ggtitle("Bachelor's Degree Education of people who votes by States") + coord_flip()
+
+bar2 <- ggplot(data=total, aes(x=State1,y=Highschoold))
+bar + geom_bar(stat="identity") + geom_text(aes(label=Highschoold), vjust=1.6, color="Blue", size=3.5)+
+  xlab("States") +  ylab("Percentage of individual with bachelor's degree or higher") + ggtitle("Bachelor's Degree Education of people who votes by States") + coord_flip()
+
+
+
+
+
+
+write.xlsx(x = total, file = "Education.xlsx", sheetName = "TestSheet", row.names = FALSE)
+write.csv(total, file = "Education.csv")
